c941475f39413b499a07212c9b5b787adc2b323f
Aris Munandar <arismun0010@gmail.com>
2022-02-24 23:14:40 +0700

    V1.1

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ export default class MyDocument extends Document {
         <Head>
           {/* use google font */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
             href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;300;400;600;700;900&display=swap"
             rel="stylesheet"
